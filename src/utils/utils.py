abccc6cd4b0422147ea2266e4c365cf9785814fc
Giorgio <jeanpierrepolnareff@protonmail.com>
2023-05-14 21:05:04 +0200

    I can't convert to equirectangular coordinates, ffs

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -9,70 +9,50 @@ from ..utils.constants import *
 
 # function to plot the satellite orbit in 3D space with the earth at the origin 
 
-def plot_groundtrack(Coords, res=(800, 800), Mode='markers+lines', proj_type='equirectangular'): 
-
+def plot_groundtrack(Sat_ID, Coords, res=(800, 800), Mode='markers+lines', style='seaborn', 
+                     marker_size=0.5, tracecolor='red', play_duration=300): 
     Lat = Coords[:, 0]
     Lon = Coords[:, 1]
-    types = ['orthographic', 'natural earth', 'kavrayskiy7', 'miller', 'robinson', 'equirectangular', 'mercator', 'orthographic', 'natural earth', 'kavrayskiy7', 'miller', 'robinson', 'equirectangular', 'mercator']
-    if proj_type not in types:
-        raise ValueError("Plot type must be picked from the plotly list of projections: \n" + str(types))
+
+    # converting latitude and longitude to equirectangular projection
+    Lat_equi = 800 * np.cos(np.deg2rad(Lat)) * np.cos(np.deg2rad(Lon))
+    Lon_equi = 400 * np.cos(np.deg2rad(Lat)) * np.sin(np.deg2rad(Lon))
+
+
+    animation_steps = 40
     
-    fig = make_subplots(rows=1, cols=2, subplot_titles=("Orthographic Projection", "Flat Projection"))
+    fig = make_subplots(rows=1, cols=2, subplot_titles=("Orthographic Projection", "Flat Projection"), specs=[[{"type": "geo"}, {"type": "geo"}]], 
+                    print_grid=True)
 
-    
-    # Define the data for your plot
-    data_ortho = [
-        go.Scattergeo(
-            lat=Lat, lon=Lon, mode=Mode, marker=dict(size=0, color='red'),
-            hoverinfo="none"
-        )
-    ]
+    fig.add_trace(go.Scattergeo(lat=Lat, lon=Lon, mode=Mode, marker=dict(size=marker_size, color=tracecolor), name=Sat_ID), row=1, col=1)
+    fig.add_trace(go.Scattergeo(lat=Lat_equi, lon=Lon_equi, mode=Mode, marker=dict(size=marker_size, color=tracecolor), showlegend=False), row=1, col=2)
 
-    data_flat = [
-        go.Scattergeo(
-            lat=Lat, lon=Lon, mode=Mode, marker=dict(size=0, color='red'),
-            hoverinfo="none"
-        )
-    ]
+    fig.update_geos(projection_type="orthographic", row=1, col=1, 
+                    showland=True, landcolor="LightGreen", showocean=True, oceancolor="#179fff", showlakes=True, lakecolor="#179fff", 
+                    showrivers=True, rivercolor="#179fff")
+    fig.update_geos(projection_type="equirectangular", row=1, col=2, 
+                    showland=True, landcolor="LightGreen", showocean=True, oceancolor="#179fff", showlakes=True, lakecolor="#179fff", 
+                    showrivers=True, rivercolor="#179fff")
+    fig.update_layout(template=style, width=res[0], height=res[1]//2, title_text="Groundtrack", showlegend=True)
 
-    # Define the layout for your plot
-    layout_ortho = go.Layout(
-        geo=dict(
-            showland=True, landcolor="Green",
-            showocean=True, oceancolor="LightBlue",
-            showcountries=False, showcoastlines=True,
-            projection=dict(type=type, rotation=dict(lat=0, lon=0, roll=0)),
-            bgcolor="black"
-        ),
-        width=res[0]//2, height=res[1], 
-        scene=dict(
-            xaxis=dict(visible=False),
-            yaxis=dict(visible=False),
-            zaxis=dict(visible=False),
-            aspectratio=dict(x=1, y=1, z=1), 
-            domain=dict(x=[0, 0.6], y=[0, 1]))
-    )
+    frames = []
+    for i in range(1, animation_steps + 1):
+        alpha = i / animation_steps
+        frames.append(go.Frame(data=[go.Scattergeo(lat=Lat[:int(alpha*len(Lat))], lon=Lon[:int(alpha*len(Lon))],
+                                                    mode=Mode, marker=dict(size=marker_size, color=tracecolor),
+                                                    name=Sat_ID)],
+                               layout=dict(geo=dict(projection_type='orthographic'))))
 
-    layout_flat = go.Layout(
-        xaxis=dict(visible=True, range=[-180, 180]),
-        yaxis=dict(visible=True, range=[-90, 90]),
-        width=res[0]//2, height=res[1],
-        margin=dict(l=0, r=0, t=40, b=0),
-        plot_bgcolor="black",
-        paper_bgcolor="black",
-    )
-    
-    # Add the traces and layouts to the figure
-    fig.add_trace(data_ortho[0], row=1, col=1)
-    fig.add_trace(data_flat[0], row=1, col=2)
-    fig.update_layout(layout_ortho, row=1, col=1)
-    fig.update_layout(layout_flat, row=1, col=2)
-    
-    # Update the flat projection subplot to use the chosen projection
-    fig.update_yaxes(scaleanchor="x", scaleratio=1)
-    fig.update_xaxes(scaleanchor="y", scaleratio=1)
-    fig.update_layout(scene=dict(projection=dict(type=proj_type)))
-
+    fig.frames = frames
+    fig.update_layout(updatemenus=[dict(type='buttons', showactive=False, buttons=[dict(label='Play',
+                                                                                      method='animate',
+                                                                                      args=[None, dict(frame=dict(duration=play_duration),
+                                                                                                        fromcurrent=True,
+                                                                                                        mode="immediate",
+                                                                                                        transition=dict(duration=0))]),
+                                                                                     dict(label='Pause',
+                                                                                          method='animate',
+                                                                                          args=[[None], dict(frame=dict(duration=0),
+                                                                                                              mode='immediate',
+                                                                                                              transition=dict(duration=0))])])])
     fig.show()
-
-
